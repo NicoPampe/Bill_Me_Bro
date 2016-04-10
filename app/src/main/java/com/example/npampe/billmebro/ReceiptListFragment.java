ff6ce2bb6bcecd780e7cd199b33664df8c5637fc
John Spielvogel <John Spielvogel>
2016-04-10 17:06:20 -0600

    CHANGE(s): 	- WOOOOOHOOOOOO! Finally got the damn expandable recycler list 	  functioning at the most basic level. Never felt better!
    TODO(s):
    	- Clean up leftover "old" RecyclerView implementation
    	- Clean up new implementation code and ensure it is ready to
    	  accept information from the model.
    		- Will need to be able to accept generated receipts and
    		  place them into their respective group (by date).
    	- May want to review new code.

--- app/src/main/java/com/example/npampe/billmebro/ReceiptListFragment.java
+++ app/src/main/java/com/example/npampe/billmebro/ReceiptListFragment.java
@@ -1,16 +1,25 @@
 package com.example.npampe.billmebro;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bignerdranch.expandablerecyclerview.Adapter.ExpandableRecyclerAdapter;
+import com.bignerdranch.expandablerecyclerview.Model.ParentListItem;
+import com.bignerdranch.expandablerecyclerview.ViewHolder.ChildViewHolder;
+import com.bignerdranch.expandablerecyclerview.ViewHolder.ParentViewHolder;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,10 +27,11 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ReceiptListFragment extends Fragment {
+    private static final String TAG = "Receipt_List_Fragment";
     @Bind(R.id.receipt_recycler_view)
     RecyclerView mRecyclerView;
 
-    private ReceiptAdapter mAdapter;
+    private MyAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,7 +40,7 @@ public class ReceiptListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_receipt_list, container, false);
         ButterKnife.bind(this, view);
 
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+       mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         updateUI();
         return view;
     }
@@ -48,60 +58,175 @@ public class ReceiptListFragment extends Fragment {
     }
 
     public void updateUI() {
-        List<Receipt> receipts = Arrays.asList(new Receipt("Receipt A"), new Receipt("Receipt B"));
+        List<Receipt> receipts0 = Arrays.asList(new Receipt("Receipt A"), new Receipt("Receipt B"));
+        List<Receipt> receipts1 = Arrays.asList(new Receipt("Receipt C"), new Receipt("Receipt D"));
+        ReceiptParentListItem item0 = new ReceiptParentListItem(receipts0);
+        ReceiptParentListItem item1 = new ReceiptParentListItem(receipts1);
+        List<ParentListItem> items = new ArrayList<ParentListItem>();
+        items.add(item0);
+        items.add(item1);
 
         if (mAdapter == null) {
-            mAdapter = new ReceiptAdapter(receipts);
+            //Log.d(TAG, "updateUI: receipts size = " + receipts.size());
+
+            mAdapter = new MyAdapter(getActivity().getApplicationContext(), items);
             mRecyclerView.setAdapter(mAdapter);
         }
     }
 
-    public class ReceiptHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private Receipt mReceipt;
-        @Bind(R.id.receipt_title_text_view)
-        TextView mTitleTextView;
+    public class ReceiptParentListItem implements ParentListItem {
+        private List mReceipts;
+        private String mDate;
 
-        public ReceiptHolder(View itemView) {
-            super(itemView);
-            itemView.setOnClickListener(this);
-            ButterKnife.bind(this, itemView);
-        }
-
-        public void bindReceipt(Receipt receipt) {
-            mReceipt = receipt;
-            mTitleTextView.setText(mReceipt.getTitle());
-        }
-
-        @Override
-        public void onClick(View v) {
-            // Do nothing
-        }
-    }
-
-    public class ReceiptAdapter extends RecyclerView.Adapter<ReceiptHolder> {
-        private List<Receipt> mReceipts;
-
-        public ReceiptAdapter(List<Receipt> receipts) {
+        public ReceiptParentListItem(List receipts) {
             mReceipts = receipts;
+            mDate = "Date 0";
         }
 
         @Override
-        public ReceiptHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            View view = layoutInflater
-                    .inflate(R.layout.list_item_receipt, parent, false);
-            return new ReceiptHolder(view);
+        public List<?> getChildItemList() {
+            return mReceipts;
         }
 
         @Override
-        public void onBindViewHolder(ReceiptHolder holder, int position) {
-            Receipt receipt = mReceipts.get(position);
-            holder.bindReceipt(receipt);
+        public boolean isInitiallyExpanded() {
+            return false;
         }
 
-        @Override
-        public int getItemCount() {
-            return mReceipts.size();
+        public String getDate() {
+            return mDate;
         }
     }
+
+    public class ReceiptParentViewHolder extends ParentViewHolder {
+        private TextView mRecipeTextView;
+        private ImageView mArrowExpandImageView;
+
+        public ReceiptParentViewHolder(View itemView) {
+            super(itemView);
+            mRecipeTextView = (TextView) itemView.findViewById(R.id.list_item_receipt_parent_text_view);
+
+            mArrowExpandImageView = (ImageView) itemView.findViewById(R.id.list_item_receipt_parent_drop_down_button);
+            mArrowExpandImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (isExpanded()) {
+                        collapseView();
+                    } else {
+                        expandView();
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean shouldItemViewClickToggleExpansion() {
+            return false;
+        }
+
+        public void bind(ReceiptParentListItem parentItem) {
+            mRecipeTextView.setText(parentItem.getDate());
+        }
+    }
+
+    public class ReceiptChildViewHolder extends ChildViewHolder {
+        private TextView mIngredientTextView;
+
+        public ReceiptChildViewHolder(View itemView) {
+            super(itemView);
+            mIngredientTextView = (TextView) itemView.findViewById(R.id.list_item_receipt_child_text_view);
+        }
+
+        public void bind(Receipt receipt) {
+            mIngredientTextView.setText(receipt.getTitle());
+        }
+    }
+
+    public class MyAdapter extends ExpandableRecyclerAdapter<ReceiptParentViewHolder, ReceiptChildViewHolder> {
+
+        private LayoutInflater mInflator;
+
+        public MyAdapter(Context context, List<ParentListItem> parentItemList) {
+            super(parentItemList);
+            mInflator = LayoutInflater.from(context);
+        }
+
+        // onCreate ...
+        @Override
+        public ReceiptParentViewHolder onCreateParentViewHolder(ViewGroup parentViewGroup) {
+            Log.d(TAG, "onCreateParentViewHolder: inflating parent holder");
+            View recipeView = mInflator.inflate(R.layout.list_item_receipt_parent, parentViewGroup, false);
+            return new ReceiptParentViewHolder(recipeView);
+        }
+
+        @Override
+        public ReceiptChildViewHolder onCreateChildViewHolder(ViewGroup childViewGroup) {
+            Log.d(TAG, "onCreateChildViewHolder: inflating child holder");
+            View ingredientView = mInflator.inflate(R.layout.list_item_receipt_child, childViewGroup, false);
+            return new ReceiptChildViewHolder(ingredientView);
+        }
+
+        // onBind ...
+        @Override
+        public void onBindParentViewHolder(ReceiptParentViewHolder receiptParentViewHolder, int position, ParentListItem parentListItem) {
+            ReceiptParentListItem receipt = (ReceiptParentListItem) parentListItem;
+            receiptParentViewHolder.bind(receipt);
+        }
+
+        @Override
+        public void onBindChildViewHolder(ReceiptChildViewHolder receiptChildViewHolder, int position, Object childListItem) {
+            Receipt receiptChild = (Receipt) childListItem;
+            receiptChildViewHolder.bind(receiptChild);
+        }
+    }
+
+//    public class ReceiptHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+//        private Receipt mReceipt;
+//        @Bind(R.id.receipt_title_text_view)
+//        TextView mTitleTextView;
+//
+//        public ReceiptHolder(View itemView) {
+//            super(itemView);
+//            itemView.setOnClickListener(this);
+//            ButterKnife.bind(this, itemView);
+//        }
+//
+//        public void bindReceipt(Receipt receipt) {
+//            mReceipt = receipt;
+//            mTitleTextView.setText(mReceipt.getTitle());
+//        }
+//
+//        @Override
+//        public void onClick(View v) {
+//            // Do nothing
+//        }
+//    }
+//    public class ReceiptAdapter extends RecyclerView.Adapter<ReceiptHolder> {
+//        private List<Receipt> mReceipts;
+//
+//        public ReceiptAdapter(List<Receipt> receipts) {
+//            Log.d(TAG, "ReceiptAdapter: receipts size = " + receipts.size());
+//            mReceipts = receipts;
+//        }
+//
+//        @Override
+//        public ReceiptHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+//            View view = layoutInflater
+//                    .inflate(R.layout.list_item_receipt, parent, false);
+//            return new ReceiptHolder(view);
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(ReceiptHolder holder, int position) {
+//            Log.d(TAG, "ReceiptAdapter::onBindViewHolder: pos = " + position);
+//            Receipt receipt = mReceipts.get(position);
+//            holder.bindReceipt(receipt);
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return mReceipts.size();
+//        }
+//    }
 }
